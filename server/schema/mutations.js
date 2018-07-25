bef24e3060f7c324f993e2f819983daa8d3d3e73
Cuong Ho <cuong.ict.10101@gmail.com>
2018-07-26 00:14:03 +0700

    login

--- server/schema/mutations.js
+++ server/schema/mutations.js
@@ -28,6 +28,16 @@ const mutationType = new GraphQLObjectType({
         req.logout;
         return user;
       }
+    },
+    login: {
+      type: UserType,
+      args: {
+        email: { type: GraphQLString },
+        password: { type: GraphQLString }
+      },
+      resolve(parentValue, { email, password }, req) {
+        return AuthService.login({ email, password, req });
+      }
     }
   }
 });
