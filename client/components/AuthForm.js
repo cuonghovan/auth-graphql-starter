1d3fa02d6c83abdbe29a58be865b808328e73d46
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-07-26 10:54:03 +0700

    handle login mutation

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -9,21 +9,28 @@ class AuthForm extends React.Component {
       email: '',
       password: ''
     }
-  }  
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state);
+  }
+
   render() {
     return (
       <div className='row'>
-        <form className='col s6'>
+        <form className='col s6' onSubmit={this.onSubmit.bind(this)}>
           <div className='input-field'>
-            <label>Email</label>
-            <input 
+            <input
+              placeholder='Email'
               value={this.state.email}
               onChange={e => this.setState({ email: e.target.value })}
             />
           </div>
           <div className='input-field'>
-            <label>Password</label>
-            <input 
+            <input
+              placeholder='Password'
               value={this.state.password}
               onChange={e => this.setState({ password: e.target.value })}
             />
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { graphql } from 'react-apollo';
+
 import AuthForm from './AuthForm';
+import mutation from '../mutations/Login';
+import query from '../queries/CurrentUser';
 
 class LoginForm extends React.Component {
 
@@ -7,11 +11,18 @@ class LoginForm extends React.Component {
     super(props);
   }
 
+  onSubmit({ email, password }) {
+    this.props.mutate({
+      variables: { email, password },
+      refetchQueries: [{ query }]
+    });
+  }
+
   render() {
     return(
       <div>
         <h3>Login</h3>
-        <AuthForm />
+        <AuthForm onSubmit={this.onSubmit.bind(this)} />
       </div>
     );
   }
@@ -19,4 +30,7 @@ class LoginForm extends React.Component {
 
 LoginForm.propTypes = {};
 
-export default LoginForm;
+export default graphql(mutation)(
+  graphql(query)(LoginForm)
+);
+    
