8999d08d09fe5bf69fe40f37096803c89ecdf391
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-07-26 08:33:47 +0700

    Root Route

---
+++ client/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>Header</div>
+    );
+  }
+}
+
+Header.propTypes = {};
+
+export default Header;
