d9c744b9540317e6a8429e4a6917adf383ff7372
Bepatient <bepatient@framgia0208-pc.framgia.com>
2018-07-26 10:11:29 +0700

    Rerender header after logout mutation

--- client/components/Header.js
+++ client/components/Header.js
@@ -12,27 +12,29 @@ class Header extends React.Component {
   }
 
   onLogoutClick() {
-    this.props.mutate({});
+    this.props.mutate({
+        refetchQueries: [{ query }]
+    });
   }
 
   renderButtons() {
     const { loading, user } = this.props.data;
 
     if (loading) {
-        return (<div />)
+      return (<div />);
     }
 
     if (user) {
       return (
-          <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
-      )
+        <li><a onClick={this.onLogoutClick.bind(this)}>Logout</a></li>
+      );
     } else {
       return (
-          <div>
-              <li><Link to='/signup'>Signup</Link></li>
-              <li><Link to='/login'>Login</Link></li>
-          </div>
-      )
+        <div>
+          <li><Link to='/signup'>Signup</Link></li>
+          <li><Link to='/login'>Login</Link></li>
+        </div>
+      );
     }
   }
 
